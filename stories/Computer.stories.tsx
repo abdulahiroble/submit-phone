051da1353d3a080f3ee13217bd1e40308fd61c1e
abdulahi roble <abdulahi_2610@hotmail.com>
2021-04-12 17:20:51 +0200

    Install typescript and storybook

---
+++ stories/Computer.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Computer } from "./Computer";
+
+export default {
+  title: "Computer",
+};
+
+export const DefaultComponent = () => {
+  return (
+    <div>
+      <Computer />
+    </div>
+  );
+};
