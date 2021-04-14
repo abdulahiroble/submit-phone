a19fe10c2ba44f3b9cd09d6b2ee92b4a2e403dcd
abdulahi roble <abdulahi_2610@hotmail.com>
2021-04-14 10:15:25 +0200

    Rows

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,22 +14,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="w-8/12 sm:max-w-lg mx-auto">
+      <div className="w-8/12 sm:w-1/2 mx-auto">
         <div className="flex flex-col sm:flex-row">
           <div className="flex-1 p-5">
-            <div className="text-center"><Computer />Få et prisoverslag på din telefon</div>
+            <div className="text-center"><Computer /><div className="mt-3">Få et prisoverslag på din telefon</div></div>
           </div>
           <div className="flex-1 p-5">
-            <div className="text-center"><Package />Send din telefon til Swappie
+            <div className="text-center"><Package /><div className="mt-3">Send din telefon til PhoneSwap</div>
             </div>
           </div>
           <div className="flex-1 p-5">
             <div className="text-center"><Payment />
-              Bankoverførsel foretages til din konto efter vores gennemtjek</div>
+              <div className="mt-3">Bankoverførsel foretages til din konto efter vores gennemtjek</div></div>
           </div>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-10">
+        <div className="grid grid-rows-3 grid-flow-row  sm:grid-flow-col gap-4 mt-10">
           <div className="flex"><CheckMark /><div className="pl-3">Fri fragt med DHL Express</div></div>
           <div className="flex"><CheckMark /><div className="pl-3">Vores bud er gyldigt i 2 uger</div></div>
           <div className="flex"><CheckMark /><div className="pl-3">Hurtig betaling til din bankkonto</div></div>
