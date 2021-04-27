4a193addb5cbb01278cc733320ff3039fca1e633
abdulahi roble <abdulahi_2610@hotmail.com>
2021-04-27 11:25:14 +0200

    Build error

--- pages/form.tsx
+++ pages/form.tsx
@@ -6,34 +6,34 @@ const form = () => {
             <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
                 <div className="-mx-3 md:flex mb-6">
                     <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
+                        <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
                             First Name
                         </label>
                         <input className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" id="grid-first-name" type="text" placeholder="Jane" />
 
                     </div>
                     <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
+                        <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
                             Last name
                         </label>
                         <input className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" id="grid-first-name" type="text" placeholder="Jane" />
 
                     </div>
                     <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
+                        <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
                             Email
                         </label>
                         <input className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" id="grid-first-name" type="text" placeholder="Jane" />
 
                     </div>
                     <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
+                        <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
                             Phone
                         </label>
                         <input className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" id="grid-first-name" type="text" placeholder="Jane" />
                     </div>
                     <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
+                        <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
                             Adress
                         </label>
                         <input className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" id="grid-first-name" type="text" placeholder="Jane" />
@@ -43,13 +43,13 @@ const form = () => {
 
                 <div className="-mx-3 md:flex mb-2">
                     <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-city">
+                        <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
                             City
                         </label>
                         <input className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" id="grid-city" type="text" placeholder="Albuquerque" />
                     </div>
                     <div className="md:w-1/2 px-3">
-                        <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-zip">
+                        <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
                             Zip
                         </label>
                         <input className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" id="grid-zip" type="text" placeholder="90210" />
